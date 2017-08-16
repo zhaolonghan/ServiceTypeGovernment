928ff0110e91202f0985620fa4eeb2a0831cebfa
zheng19930310 <1170818821@qq.com>
2017-08-16 17:36:19 +0800

    test

---
+++ app/src/main/java/wancheng/com/servicetypegovernment/util/Test.java
@@ -0,0 +1,12 @@
+package wancheng.com.servicetypegovernment.util;
+
+/**
+ * Created by john on 2017/8/16.
+ */
+public class Test {
+
+
+    public static void main(String[] arg1){
+        System.out.print("hello world!");
+    }
+}
