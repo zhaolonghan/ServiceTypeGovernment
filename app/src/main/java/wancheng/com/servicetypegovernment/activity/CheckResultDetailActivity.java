ff2c14ec6ef62df0143e37f92f54b3544affebad
zheng19930310 <1170818821@qq.com>
2017-08-30 20:17:27 +0800

    检查历史

--- app/src/main/java/wancheng/com/servicetypegovernment/activity/CheckResultDetailActivity.java
+++ app/src/main/java/wancheng/com/servicetypegovernment/activity/CheckResultDetailActivity.java
@@ -4,20 +4,33 @@ package wancheng.com.servicetypegovernment.activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import wancheng.com.servicetypegovernment.R;
 import wancheng.com.servicetypegovernment.bean.TopBean;
 
 public class CheckResultDetailActivity extends BaseActivity {
-
+    private ScrollView view_1layout;
+    private LinearLayout view_2layout;
+    private ScrollView view_3layout;
+    private RelativeLayout relNewsListName;//
+    private RelativeLayout relNoticeListName;//
+    private RelativeLayout relLawListName;//
+    private TextView tvNew;
+    private TextView tvlNotice;
+    private TextView tvlLaw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +46,82 @@ public class CheckResultDetailActivity extends BaseActivity {
 //            }
 //        });
 
-        TopBean topBean=new TopBean("检查详情","返回","下一步",true,true);
+        TopBean topBean=new TopBean("检查详情","返回","下一步",true,false);
         getTopView(topBean);
 
+        tvlNotice =(TextView)this.findViewById(R.id.tv_notice);
+        tvlLaw=(TextView)this.findViewById(R.id.tv_loyal);
+        tvNew=(TextView)this.findViewById(R.id.tv_news);
+        relNoticeListName=(RelativeLayout)this.findViewById(R.id.listname_notice);
+        relNewsListName=(RelativeLayout)this.findViewById(R.id.listname_news);
+        relLawListName=(RelativeLayout)this.findViewById(R.id.listname_law);
 
+        final ColorStateList btnblue=tvlNotice.getTextColors();
+        final  ColorStateList btnblack=tvNew.getTextColors();
+        final Drawable linered=relNoticeListName.getChildAt(1).getBackground();
+        final   Drawable lineblack=relNewsListName.getChildAt(1).getBackground();
+
+
+        view_1layout= (ScrollView)findViewById(R.id.view_1);
+        view_2layout= (LinearLayout)findViewById(R.id.view_2);
+        view_3layout= (ScrollView)findViewById(R.id.view_3);
+        view_1layout.setVisibility(View.VISIBLE);
+        view_2layout.setVisibility(View.GONE);
+        view_3layout.setVisibility(View.GONE);
+        relNoticeListName.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                tvNew.setTextColor(btnblack);
+                tvlNotice.setTextColor(btnblue);
+                tvlLaw.setTextColor(btnblack);
+                //线
+
+                relNewsListName.getChildAt(1).setBackground(lineblack);
+                relNoticeListName.getChildAt(1).setBackground(linered);
+                relLawListName.getChildAt(1).setBackground(lineblack);
+                view_1layout.setVisibility(View.VISIBLE);
+                view_2layout.setVisibility(View.GONE);
+                view_3layout.setVisibility(View.GONE);
+
+
+            }
+        });
+
+
+        relLawListName.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                tvNew.setTextColor(btnblack);
+                tvlNotice.setTextColor(btnblack);
+                tvlLaw.setTextColor(btnblue);
+                //线
+                relNewsListName.getChildAt(1).setBackground(lineblack);
+                relNoticeListName.getChildAt(1).setBackground(lineblack);
+                relLawListName.getChildAt(1).setBackground(linered);
+                view_2layout.setVisibility(View.VISIBLE);
+                view_1layout.setVisibility(View.GONE);
+                view_3layout.setVisibility(View.GONE);
+
+            }
+        });
+        //新闻
+        relNewsListName.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // footView(2);
+                tvNew.setTextColor(btnblue);
+                tvlNotice.setTextColor(btnblack);
+                tvlLaw.setTextColor(btnblack);
+                //线
+
+                relNewsListName.getChildAt(1).setBackground(linered);
+                relNoticeListName.getChildAt(1).setBackground(lineblack);
+                relLawListName.getChildAt(1).setBackground(lineblack);
+                view_3layout.setVisibility(View.VISIBLE);
+                view_2layout.setVisibility(View.GONE);
+                view_1layout.setVisibility(View.GONE);
+
+            }
+        });
     }
 
 
